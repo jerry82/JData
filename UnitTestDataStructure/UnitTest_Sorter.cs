fbd122c836f7ba0b009c8f2b0b623c90b8f764cc
jerry82 <hatrungn@yahoo.com>
2014-01-03 10:08:56 -0500

    init checkin

---
+++ UnitTestDataStructure/UnitTest_Sorter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JDataStructure.Sort;
+
+namespace UnitTestDataStructure
+{
+    [TestClass]
+    public class UnitTest_Sorter
+    {
+        [TestMethod]
+        public void TestBubbleSort()
+        {
+            BubbleSort sorter = new BubbleSort();
+            List<int> arr = new List<int>() { 4, 5, 3, 1, 2 };
+            
+            List<int> sortedArr = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> sortedArrDesc = new List<int>() { 5, 4, 3, 2, 1 };
+
+            sorter.Ascend(arr);
+            CollectionAssert.AreEqual(sortedArr, arr);
+
+            sorter.Descend(arr);
+            CollectionAssert.AreEqual(sortedArrDesc, arr);
+        }
+
+        [TestMethod]
+        public void TestQuickSortAsc()
+        {
+            QuickSort sorter = new QuickSort();           
+            List<int> arr = new List<int>() { 5, 5, 3, 2, 1 };
+            List<int> sortedArr = new List<int>() { 1, 2, 3, 5, 5 };
+
+            sorter.Ascend(arr);
+            CollectionAssert.AreEqual(sortedArr, arr);
+        }
+    }
+}
---
+++ UnitTestDataStructure/UnitTest_Trie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JDataStructure;
+
+namespace UnitTestDataStructure
+{
+    [TestClass]
+    public class UnitTest_Trie
+    {
+        JTrie _dict;
+
+        [TestInitialize]
+        public void InitTrie()
+        {
+            string[] paths = { "dict/web2", "dict/web2a" };
+            _dict = new JTrie();
+            string tmp = String.Empty;
+
+            foreach (string path in paths)
+            {
+                using (StreamReader sr = new StreamReader(path, Encoding.ASCII))
+                {
+                    while ((tmp = sr.ReadLine()) != null)
+                    {
+                        _dict.Add(tmp);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestTrie()
+        {
+            List<string> result = _dict.SearchPrefix("a", 10);
+            Assert.AreEqual(10, result.Count);
+        }
+    }
+}
