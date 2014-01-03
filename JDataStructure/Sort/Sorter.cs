6b16f5bac9a2c5edf64a1fda54eb519dd890c365
jerry <hatrungn@gmail.com>
2014-01-03 10:42:32 -0500

    init

---
+++ JDataStructure/Sort/Sorter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDataStructure.Sort
+{
+    public abstract class Sorter
+    {
+        public abstract void Ascend(List<int> arr);
+        public virtual void Descend(List<int> arr)
+        {
+
+        }
+    }
+
+    public class BubbleSort : Sorter
+    {
+        public override void Ascend(List<int> arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class QuickSort : Sorter
+    {
+        public override void Ascend(List<int> arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MergeSort : Sorter
+    {
+        public override void Ascend(List<int> arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
